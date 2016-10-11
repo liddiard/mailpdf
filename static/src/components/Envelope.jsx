f32064d5ea0cdbf950e909f1756e8bdeb91f88ca
omniaura <omniaura5@gmail.com>
2016-10-11 13:42:56 -0700

    add container around envelope for checkout success msg

--- static/src/components/Envelope.jsx
+++ static/src/components/Envelope.jsx
@@ -9,17 +9,43 @@ export default class Envelope extends React.Component {
     this.state = {};
   }
 
+  reloadPage() {
+    location.reload();
+  }
+
   render() {
+    const animationClass = this.props.sentSuccessfully ? 'bounceOutRight' : 'slideInUp';
+    const sendSuccessClass = this.props.sentSuccessfully ? 'animated fadeIn' : 'hidden';
     return (
-      <div id="envelope" className="animated slideInUp">
-        <img className="stamp" src="/img/stamp.png" />
-        <Address from={true} updateAddress={this.props.updateAddress} />
-        <Address from={false} updateAddress={this.props.updateAddress} />
+      <div id="envelope-container">
+        <div id="envelope" className={`animated ${animationClass}`}>
+          <img className="stamp" src="/img/stamp.png" />
+          <Address from={true} fields={this.props.fromFields}
+                  fileUploadHasBegun={this.props.fileUploadHasBegun}
+                  updateAddress={this.props.updateAddress} />
+          <Address from={false} fields={this.props.toFields}
+                  fileUploadHasBegun={this.props.fileUploadHasBegun}
+                  updateAddress={this.props.updateAddress} />
+        </div>
+        <div id="send-success" className={sendSuccessClass}>
+          <div>
+            <h2>All set!</h2>
+            <p>Your document has begun its journey to {this.props.toFields.line1}.</p>
+            <p>You will receive an email shortly with tracking information.</p>
+            <button className="send-another" onClick={this.reloadPage}>
+              Send Another <i className="fa fa-refresh" aria-hidden="true"></i>
+            </button>
+          </div>
+        </div>
       </div>
     )
   }
 }
 
 Envelope.propTypes = {
-  updateAddress: React.PropTypes.func.isRequired
+  fileUploadHasBegun: React.PropTypes.bool.isRequired,
+  fromFields: React.PropTypes.object.isRequired,
+  toFields: React.PropTypes.object.isRequired,
+  updateAddress: React.PropTypes.func.isRequired,
+  sentSuccessfully: React.PropTypes.bool.isRequired
 }
