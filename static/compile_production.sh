b208fbda5ac5469d899ca557815223ccef7f3b9c
omniaura <omniaura5@gmail.com>
2016-10-12 11:09:55 -0700

    verbose static compile script

--- static/compile_production.sh
+++ static/compile_production.sh
@@ -3,7 +3,11 @@
 # http://stackoverflow.com/a/16349776/2487925
 cd "${0%/*}"
 
+echo "Installing browserify..."
 npm install -g browserify
+echo "Installing dev dependencies..."
 npm install --only=dev
+echo "Installing production dependencies..."
 npm install --only=production
+echo "Building browserify bundle..."
 browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify -o dist/bundle.js src/app.jsx 
