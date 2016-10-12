9bedbf282f2da297c3bdb383765b7575d97ed2cd
omniaura <omniaura5@gmail.com>
2016-10-12 10:43:50 -0700

    install browserify globally

--- static/compile_production.sh
+++ static/compile_production.sh
@@ -3,6 +3,7 @@
 # http://stackoverflow.com/a/16349776/2487925
 cd "${0%/*}"
 
+npm install -g browserify
 npm install --only=dev
 npm install --only=production
 browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify -o dist/bundle.js src/app.jsx 
