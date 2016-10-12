1a4ec1b986d0895a7c72a74f4a0048aa8a6c1af8
omniaura <omniaura5@gmail.com>
2016-10-12 10:29:22 -0700

    make script run in current directory

--- static/compile_production.sh
+++ static/compile_production.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
+# http://stackoverflow.com/a/16349776/2487925
+cd "${0%/*}"
+
 npm install
 browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify -o dist/bundle.js src/app.jsx 
