21a998c7179c4cf2cd99965fad18d839c4b814f2
omniaura <omniaura5@gmail.com>
2016-10-13 09:24:47 -0700

    changed bucket name, proxy s3 through cloudflare

--- static/deploy.sh
+++ static/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bucket='s3://mailpdfonline/'
+bucket='s3://static.mailpdf.online/'
 
 echo "Building release browserify bundle..."
 browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify src/app.jsx | aws s3 cp - "${bucket}dist/bundle.js"
