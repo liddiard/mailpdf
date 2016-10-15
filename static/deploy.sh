a21681f15e57f67bc1c09784662fbbc48749cbde
omniaura <omniaura5@gmail.com>
2016-10-15 12:38:26 -0700

    specify gzipped bundle on s3 upload

--- static/deploy.sh
+++ static/deploy.sh
@@ -3,5 +3,5 @@
 bucket='s3://static.mailpdf.online/'
 
 echo "Building release browserify bundle..."
-browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify src/app.jsx | gzip | aws s3 cp - "${bucket}dist/bundle.js"
+browserify -t [ babelify --presets [ es2015 react ] ] -t scssify -t [ envify --NODE_ENV production ] -t uglifyify src/app.jsx | gzip | aws s3 cp --content-encoding=gzip - "${bucket}dist/bundle.js"
 echo "Uploaded bundle to S3."
